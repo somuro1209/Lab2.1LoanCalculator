3992750a8879c4242f48ca72b3d9fbcd44cde949
somuro1209 <angharckleong@hotmail.com>
2017-10-31 16:04:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/lab21loancalculator/ResultActivity.java
@@ -0,0 +1,28 @@
+package com.example.user.lab21loancalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ResultActivity extends AppCompatActivity {
+
+    //TODO : Declare global variables here
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        //TODO : Link UI to program
+        //Get input data from another activity
+        Intent intent = getIntent();
+        String status = intent.getStringExtra(MainActivity.LOAN_STATUS);
+        double payment = intent.getDoubleExtra(MainActivity.LOAN_PAYMENT, 0);
+
+        //TODO :
+    }
+
+    public void closeActivity(View view){
+        finish();
+    }
+}
